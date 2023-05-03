84396aea401c9067d635ac0950f545c5b1a5a314
adamalbu <45970222+adamalbu@users.noreply.github.com>
2023-05-03 19:02:14 +0100

    Fixed invalid number error

--- python/workout/ex1/ex1_guessing_game.py
+++ python/workout/ex1/ex1_guessing_game.py
@@ -1,6 +1,13 @@
 import random
 
-guess_amount_difficulty = input("Choose a difficulty level (1, 2, or 3): ")
+while True:
+    try:
+        guess_amount_difficulty = int(input("Choose a difficulty level (1, 2, or 3): "))
+        break
+    except ValueError:
+        print("Please enter a number.")
+        continue
+
 
 def guessing_game(difficulty: int):
     target_number = random.randint(0, 100)
@@ -13,7 +20,14 @@ def guessing_game(difficulty: int):
         guesses = 3
 
     while guessed_number != target_number:
-        guessed_number = int(input("Guess a number between 0 and 100: "))
+        while True:
+            try:
+                guessed_number = int(input("Guess a number between 0 and 100: "))
+                break
+            except ValueError:
+                print("Please enter a number.")
+                continue
+
         if guesses == 0:
             return False
         guesses -= 1
@@ -24,7 +38,9 @@ def guessing_game(difficulty: int):
         else:
             return True
 
-game_result = guessing_game(int(guess_amount_difficulty))
+
+game_result = guessing_game(guess_amount_difficulty)
+
 if game_result:
     print("You won!")
 else:
