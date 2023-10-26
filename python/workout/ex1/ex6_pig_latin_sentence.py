478e5956905b5e8ce1b6241bf8c64235dd66dae1
adamalbu <45970222+adamalbu@users.noreply.github.com>
2023-10-26 19:04:03 +0100

    Done ex-1.6

--- python/workout/ex1/ex6_pig_latin_sentence.py
+++ python/workout/ex1/ex6_pig_latin_sentence.py
@@ -1,1 +1,12 @@
+from ex5_pig_latin import to_pig_latin
 
+sentence = input("Enter a sentence to convert to pig latin (only letters): ")
+split_sentence = sentence.split()
+translated_sentence = []
+
+for word in split_sentence:
+    translated_sentence.append(to_pig_latin(word))
+
+translated_sentence = ' '.join(translated_sentence)
+
+print(translated_sentence)
