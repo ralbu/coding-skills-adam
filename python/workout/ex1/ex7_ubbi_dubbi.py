37d8116de65c8e4e3adaf63dbf9194a350a6857b
adamalbu <45970222+adamalbu@users.noreply.github.com>
2023-10-27 11:26:22 +0100

    Auto stash before merge of "solutions" and "master"

--- python/workout/ex1/ex7_ubbi_dubbi.py
+++ python/workout/ex1/ex7_ubbi_dubbi.py
@@ -1,6 +1,26 @@
+vowels = ['a', 'e', 'i', 'o', 'u']
+
+
 def ubbi_dubbi_word(word):
-  pass
+    # ub before vowels
+    translated_word = []
+    for character in word:
+        if character in vowels:
+            translated_word.append('ub' + character)
+        else:
+            translated_word.append(character)
+
+    translated_word = ''.join(translated_word)
+    return translated_word
+
 
 def ubbi_dubbi_sentence(sentence):
-  pass
+    split_sentence = sentence.split()
+    translated_sentence = []
 
+    for word in split_sentence:
+        translated_sentence.append(ubbi_dubbi_word(word))
+
+    translated_sentence = ' '.join(translated_sentence)
+
+    return translated_sentence
