b3cf64b2d445db4cec957fbec8e96930c317ecde
Adam Albu <adamalbu@noreply.codeberg.org>
2026-08-26 19:07:42 +0100

    ex 17 solution

--- python/workout/part4/ex17_how_many.py
+++ python/workout/part4/ex17_how_many.py
@@ -1,8 +1,7 @@
-def how_many_different_numbers(numbers):
+def how_many_different_numbers(numbers: list[int]):
+    return len(set(numbers))
 
 def test_how_many_different_numbers():
     numbers = [1, 2, 3, 1, 1, 3, 4, 1, 3, 4]
 
     assert how_many_different_numbers(numbers) == 4
-
-
